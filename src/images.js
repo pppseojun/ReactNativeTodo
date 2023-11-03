fb3146ab3b8d33f8c8b0a0c319842c55b9238807
pppseojun <135473205+pppseojun@users.noreply.github.com>
2023-11-03 16:34:54 +0900

    first commit

---
+++ src/images.js
@@ -0,0 +1,11 @@
+import CheckBoxOutLine from '../assets/icons/check_box_outline.png';
+import CheckBox from '../assets/icons/check_box.png';
+import DeleteForever from '../assets/icons/delete_forever.png';
+import Edit from '../assets/icons/edit.png';
+
+export const images = {
+    uncompleted: CheckBoxOutLine,
+    completed: CheckBox,
+    delete: DeleteForever,
+    update: Edit,
+};
